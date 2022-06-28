dd13338f74bfd2f637c35a84a5394e8d4e0c53c2
조경민 <hatena@siliconcube.co.kr>
2022-06-28 09:59:19 +0900

    초기 커밋.

---
+++ Hub/EmptyHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace TestHub
+{
+    public class EmptyHub : Hub
+    {
+        private readonly ILogger<EmptyHub> _logger;
+
+        public EmptyHub(ILogger<EmptyHub> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            _logger.LogInformation("Connected");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+            _logger.LogInformation("Disconnected");
+        }
+    }
+}
---
+++ TestClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace TestClient
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            HubConnection hubcon = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5101/hub/empty")
+                .Build();
+
+            await hubcon.StartAsync();
+            Console.WriteLine("Connected");
+
+            Console.Read();
+        }
+    }
+}
